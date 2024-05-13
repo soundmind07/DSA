be1ad7e4abbdf7e8ae6f256df467279d8c7b03c3
Pallavit Saxena <121334016+soundmind07@users.noreply.github.com>
2024-05-13 14:52:28 +0530

    Create Program1.cpp

---
+++ Binary Search/Program1.cpp
@@ -0,0 +1,20 @@
+[Problem Link](https://leetcode.com/problems/binary-search/description/)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int left=0,right=n-1,mid;
+        while(left<=right)
+        {
+            mid=left+(right-left)/2;
+            if(nums[mid]==target)
+            return mid;
+            else if(nums[mid]>target)
+            right=mid-1;
+            else
+            left=mid+1;
+        }
+        return -1;
+    }
+};
