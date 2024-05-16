aef31cc58201e16b2c7269f1d0260c0f4b5f8156
Pallavit Saxena <121334016+soundmind07@users.noreply.github.com>
2024-05-16 14:42:37 +0530

    Create program4.cpp

---
+++ Array/program4.cpp
@@ -0,0 +1,29 @@
+find a subarray with given sum works for both positive and negative
+code:-
+  #include <bits/stdc++.h>
+using namespace std;
+
+bool isSum(int arr[], int n, int sum)
+{
+    unordered_set<int> s;
+    int pre_sum = 0;
+    for(int i = 0; i < n; i++)
+    {   
+        pre_sum += arr[i];
+        if(pre_sum==sum)
+            return true;
+        if(s.find(pre_sum-sum) != s.end())
+          return true;
+        s.insert(pre_sum);
+    }
+    return false;
+}
+
+int main()
+{
+    int arr[] = {5, 8, 6, 13, 3, -1};
+    int sum=22;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    cout << isSum(arr, n, sum);
+}
