29b3dfccba2ca43d0dec9865c1458132840c1d8c
Pallavit Saxena <121334016+soundmind07@users.noreply.github.com>
2024-05-16 15:11:10 +0530

    Create program9.cpp

---
+++ Tree/program9.cpp
@@ -0,0 +1,17 @@
+problem link:-
+https://leetcode.com/problems/symmetric-tree/description/
+  code:-
+    class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    return (left->val == right->val) && isMirror(left->left, right->right) && isMirror(left->right, right->left);
+}
+
+bool isSymmetric(TreeNode* root) {
+    if (!root) return true;
+    return isMirror(root->left, root->right);
+}
+
+};
